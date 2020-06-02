2d2cce46a8049d70681241fb6fc22a4aa026bc93
reijunior1 <reijunior1@hotmail.com>
2020-06-01 23:32:47 -0300

    Implementado Queue / LinkedList

--- src/main/java/aulas/list/ExemploLinkedList.java
+++ src/main/java/aulas/list/ExemploLinkedList.java
@@ -1,5 +1,6 @@
 package aulas.list;
 
+import java.util.Iterator;
 import java.util.Queue;
 import java.util.LinkedList;
 
@@ -14,7 +15,33 @@ public class ExemploLinkedList {
         filaBanco.add("Pat");
         filaBanco.add("Chris");
 
+        filaBanco.add("Rubi");
+
+
+        String clienteASerAtendido = filaBanco.poll();// .poll remove o primeiro elemento da lista
+        System.out.println(clienteASerAtendido); // cliente e a ser atendido
         System.out.println(filaBanco);
-        
+
+        String primeiroCliente = filaBanco.peek();// .peek apenas retorna os elementos da lista
+        System.out.println(primeiroCliente);
+        System.out.println(filaBanco);
+
+        String primeiroClienteOuErro = filaBanco.element();// retornando erro quando não há elementos na lista
+
+        System.out.println(primeiroClienteOuErro);
+        System.out.println(filaBanco);
+
+        for (String cliente : filaBanco) {
+            System.out.println("->>" + cliente);
+
+        }
+
+        Iterator<String> iteratorFilaBanco = filaBanco.iterator();
+
+        while (iteratorFilaBanco.hasNext()){
+            System.out.println("-->>" + iteratorFilaBanco.next());
+        }
+        System.out.println(filaBanco.size()); // quantidade de elementos na fila
+        System.out.println(filaBanco.isEmpty()); // retornando boolean se a lista está vazia.
     }
 }
