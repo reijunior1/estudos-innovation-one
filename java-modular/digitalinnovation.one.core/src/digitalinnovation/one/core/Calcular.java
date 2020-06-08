348f56a4c1aa8d6963051728fea75dea6bd7eb2f
reijunior1 <reijunior1@hotmail.com>
2020-06-07 22:15:10 -0300

    Update Calcular.java

--- java-modular/digitalinnovation.one.core/src/digitalinnovation/one/core/Calcular.java
+++ java-modular/digitalinnovation.one.core/src/digitalinnovation/one/core/Calcular.java
@@ -5,6 +5,6 @@ import digitalinnovation.one.core.utils.operacao.Calculadora;
 public class Calcular {
     public static void main(String[] args) {
         Calculadora calculadora = new Calculadora();
-        System.out.println((calculadora.soma(6,1)));
+        System.out.println((calculadora.mult(6,1)));
     }
 }
